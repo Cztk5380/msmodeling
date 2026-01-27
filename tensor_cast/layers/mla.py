correct the calculation of mla
Co-authored-by: lutean<lutean1@huawei.com>



# message auto-generated for no-merge-commit merge:
!54 merge develop into develop

correct the calculation of mla

Created-by: lutean
Commit-by: lutean
Merged-by: ascend-robot
Description: correct the calculation of mla

See merge request: Ascend/msmodeling!54

tensor_cast/layers/mla.py
@@ -156,12 +156,6 @@ class MultiheadLatentAttentionTensorCast(MultiheadLatentAttentionBase):
         # TODO: do inplace rotary embedding and save this torch.cat? Perhaps we can do this in graph rewrites?
         q_states = torch.cat((q_pass, q_rot), dim=-1)
 
-        # TODO: support quantization
-        if attention_meta is not None:
-            torch.ops.tensor_cast.concat_and_cache_mla(
-                kv_c_normed, k_rot, kv_cache, attention_meta.slot_mapping
-            )
-
         query_start_loc = attention_meta.query_start_loc if attention_meta else None
         seq_lens = attention_meta.seq_lens if attention_meta else None
         query_lens = attention_meta.query_lens if attention_meta else None
@@ -186,12 +180,16 @@ class MultiheadLatentAttentionTensorCast(MultiheadLatentAttentionBase):
                 quant_config.kv_offset,
                 out_dtype,
             )
+
+            if attention_meta is not None:
+                torch.ops.tensor_cast.concat_and_cache_mla(
+                    kv_c_normed, k_rot, kv_cache, attention_meta.slot_mapping
+                )
+
             # we wrap the attention operation in a custom op since it behaves differently
             # between prefill and decode shapes
             attn_output = torch.ops.tensor_cast.multihead_latent_attention_quant(
                 q_states,
-                kv_c_normed,
-                k_rot,
                 kv_cache,
                 attention_meta.block_table_tensor
                 if attention_meta is not None
@@ -222,12 +220,15 @@ class MultiheadLatentAttentionTensorCast(MultiheadLatentAttentionBase):
                 out_dtype=hidden_states.dtype,
             )
         else:
+            if attention_meta is not None:
+                torch.ops.tensor_cast.concat_and_cache_mla(
+                    kv_c_normed, k_rot, kv_cache, attention_meta.slot_mapping
+                )
+
             # we wrap the attention operation in a custom op since it behaves differently
             # between prefill and decode shapes
             attn_output = torch.ops.tensor_cast.multihead_latent_attention(
                 q_states,
-                kv_c_normed,
-                k_rot,
                 kv_cache,
                 attention_meta.block_table_tensor
                 if attention_meta is not None